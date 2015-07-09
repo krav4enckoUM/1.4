953b4e9782979ec287aebf3c6859b75dbbd39a5c
krav4enckoUM <iurakrav4encko@yandex.ru>
2015-07-09 18:41:48 +0500

    код

---
+++ Предложения/Предложения/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_3_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence; //Введенное предложение
+            Console.Write("Введите предложение: ");
+            sentence = Console.ReadLine();
+
+            sentence = sentence.TrimEnd(new char[] { '.', '!', '?' }); //Удаляем знак препинания в конце предложения.
+            //Разделяем предложение на слова и помещаем их в массив
+            string[] words = sentence.Split(
+                new string[] { " ", ", ", ". ", " - ", ": ", "; " }, System.StringSplitOptions.RemoveEmptyEntries);
+
+            //Сортируем слова по возрастанию кол-ва символов
+            IEnumerable<string> query = from word in words //Берем слова из массива words
+                                        orderby word.Length descending //Сортируем по убыванию по длине слова
+                                        select word;
+
+            foreach (string str in query)
+                Console.WriteLine(str); //Выводим итог
+
+            Console.ReadKey();
+        }
+    }
+}
